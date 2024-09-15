6e675f4e6c5b584a406f9def947cfb88eddf8594
AndyValdes94 <andyvalesn@gmail.com>
2024-09-14 21:46:01 -0400

    Update GitHomework.java

--- src/demo/GitHomework.java
+++ src/demo/GitHomework.java
@@ -4,4 +4,11 @@ public class GitHomework {
 	public void display() {
 		System.out.println("John Cena");
 	}
+
+	/**
+this is coming from GitHub
+	java is fun
+	git es easy
+	Api is not fun 
+	*?
 }
