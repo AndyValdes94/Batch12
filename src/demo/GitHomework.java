d6583c5738878a67b71f123d4ee741d53a1b47a8
Andy <Andy@DESKTOP-5FEM4B2.attlocal.net>
2024-09-14 21:58:39 -0400

    ssssssss

--- src/demo/GitHomework.java
+++ src/demo/GitHomework.java
@@ -1,14 +1,6 @@
 package demo;
 
 public class GitHomework {
-	public void display() {
-		System.out.println("John Cena");
-	}
 
-	/**
-this is coming from GitHub
-	java is fun
-	git es easy
-	Api is not fun 
-	*?
 }
+
