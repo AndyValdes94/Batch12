0ae3056963356383f9bcbb1ca8ff8d4d326cd021
Andy <Andy@DESKTOP-5FEM4B2.attlocal.net>
2024-09-14 20:11:50 -0400

    asdas

--- src/demo/GitHomework.java
+++ src/demo/GitHomework.java
@@ -2,26 +2,5 @@ package demo;
 
 public class GitHomework {
 	
-	public static void main(String[] args) {
-		System.out.println("Batch12: Git Class Lecture");
-		int age = 12;
-		String = John;
-System.out.println(age);
-		System.out.println(String);
-		//this is coming from github
-	}
 
-	
-	public static void add(int a, int b) {
-		System.out.println(a+b);
-	}
-	
-	public static void add (String a, String b) {
-		System.out.println(a + b);
-	}
-	
-	public static void substract(int a , int b) {
-		System.out.println(a+b);
-	}
-	
 }
